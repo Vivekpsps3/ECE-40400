ddf1c3cbde879ab99ab344bc4b7d7194d220206e
vivekpsps3 <vivek.panchagnula@gmail.com>
2024-02-27 08:29:47 -0500

    hw6 submit 1

--- Homework/HW06/breakRSA.py
+++ Homework/HW06/breakRSA.py
@@ -39,41 +39,53 @@ def generate_keys() -> None:
     return p,q
 
 def encrypt(message, enc1, enc2, enc3, nFile):
+    #open the files for writing
     cipher_text = [open(enc1, 'w'), open(enc2, 'w'), open(enc3, 'w')]
     n_file = open(nFile, 'w')
 
+    #for each of the files...
     for i in range(3):
+        #generate a new set of p, q, and n
         p,q = generate_keys()
         n = p * q
-        pubKey = [e,n]
-        bv_e = BitVector(intVal=e)
-        phi = (p-1)*(q-1)
-        bv_phi = BitVector(intVal=phi)
-        bv_e_i = bv_e.multiplicative_inverse(bv_phi)
-        e_i = bv_e_i.int_val()
-        d = e_i % phi
-        priKey = [d,n]
+        #create the public and private keys
+        # pubKey = [e,n]
+        # bv_e = BitVector(intVal=e)
+        # phi = (p-1)*(q-1)
+        # bv_phi = BitVector(intVal=phi)
+        # bv_e_i = bv_e.multiplicative_inverse(bv_phi)
+        # e_i = bv_e_i.int_val()
+        # d = e_i % phi
+        # priKey = [d,n]
+        #private key doesn't get used in this function but it's here for reference
+        #write the n to the n file
         n_file.write(str(n) + "\n")
+        #open the message file and read it in 128 bit blocks
         bv = BitVector(filename=message)
         while(bv.more_to_read):
             bitvec = bv.read_bits_from_file(128)
             bitvec.pad_from_right(128 - bitvec.length())
             bitvec.pad_from_left(128)
-            c = pow(bitvec.int_val(), e, pubKey[1])
+            #encrypt the block
+            c = pow(bitvec.int_val(), e, n)
             bv_c = BitVector(intVal=c, size=256)
+            #write the encrypted block to the file
             cipher_text[i].write(bv_c.get_bitvector_in_hex())
-        
+    #close and cleanup
     cipher_text[0].close()
     cipher_text[1].close()
     cipher_text[2].close()
     n_file.close()
 
-    pass
-
 def crt(bv1, bv2, bv3, N, n_list) -> int:
+    #use the chinese remainder theorem to decrypt the block
+
+    #calculate n1, n2, and n3 which are the product of all the n's except the current one
     n1 = n_list[1] * n_list[2]
     n2 = n_list[2] * n_list[0]
     n3 = n_list[0] * n_list[1]
+
+    #calculate the modular multiplicative inverse of n1, n2, and n3
     bv_n1 = BitVector(intVal=n1)
     bv_n2 = BitVector(intVal=n2)
     bv_n3 = BitVector(intVal=n3)
@@ -81,36 +93,50 @@ def crt(bv1, bv2, bv3, N, n_list) -> int:
     n2_inv = bv_n2.multiplicative_inverse(BitVector(intVal=n_list[1])).int_val()
     n3_inv = bv_n3.multiplicative_inverse(BitVector(intVal=n_list[2])).int_val()
 
+    #calculate x, which is the decrypted block
+    #x is the sum of the product of the encrypted block and the product of the n's and the modular multiplicative inverse of the n's
     x = (bv1 * n1 * n1_inv + bv2 * n2 * n2_inv + bv3 * n3 * n3_inv) % N
 
     return x
 
 def crack(enc1, enc2, enc3, nFile, cracked):
+    #open the file for writing
     cracked_file = open(cracked, 'w')
+
+    #read the n's from the file
     n_list = []
-    n = 1
+    N = 1
     with open(nFile, 'r') as file:
         for line in file:
             n_list.append(int(line))
-            n *= int(line)
+            N *= int(line)
+    
+    #open the encrypted files and read them into hex strings
     enc_list = [enc1, enc2, enc3]
-
     for i in range(3):
         enc_list[i] = BitVector(hexstring = open(enc_list[i], 'r').read())
+    
+    #iterate through the file and decrypt each block
+    #I used a counter to iterate through the file and decrypt each block again
+    #instead of using more_to_read because it seems that more_to_read is not working when reading from a hex file
     a = 0
-    b = 256
-    while(enc_list[0].length() >= b):
-        bv1 = enc_list[0][a:b]
-        bv2 = enc_list[1][a:b]
-        bv3 = enc_list[2][a:b]
+    while(enc_list[0].length() >= a + 256):
+        #decrypt the block
+        #get the next 256 bits from each file
+        bv1 = enc_list[0][a:a+256]
+        bv2 = enc_list[1][a:a+256]
+        bv3 = enc_list[2][a:a+256]
+        #increment a by 256
         a += 256
-        b += 256
-        x = crt(bv1.int_val(), bv2.int_val(), bv3.int_val(), n, n_list)
+        #decrypt the block
+        x = crt(bv1.int_val(), bv2.int_val(), bv3.int_val(), N, n_list)
+        #take the cube root of the decrypted block
         pRt = solve_pRoot(3, x)
+        #write the decrypted block to the file
         bv_pRt = BitVector(intVal=pRt, size=256)
+        #remove the padding and write the block to the file
         bv_pRt = bv_pRt[128:]
         cracked_file.write(bv_pRt.get_bitvector_in_ascii())
-    pass
 
 if __name__ == '__main__':
     if sys.argv[1] == '-e':
@@ -130,4 +156,4 @@ if __name__ == '__main__':
         crack(enc1, enc2, enc3, nFile, cracked)
 
     else:
-        print("Error invalid format given!")
+        print("Error invalid format.")
--- Homework/HW06/rsa.py
+++ Homework/HW06/rsa.py
@@ -7,31 +7,44 @@ import sys
 from BitVector import *
 from PrimeGenerator import *
 
+#global variables to store p and q file names
+#This will allow the user to specify the file names for p and q
+global p_file
+global q_file
+e=65537
 
 class RSA():
     def __init__(self,e) -> None:
         self.e = e
+        #read p and q from file if no file, then set as 0.
         try:
-            with open("p.txt", "r") as f:
+            with open(p_file, "r") as f:
                 self.p = int(f.read())
-            with open("q.txt", "r") as f:
+            with open(q_file, "r") as f:
                 self.q = int(f.read())
         except:
-            print("p.txt and/or q.txt not found. Set to 3,5 for the time being.")
-            self.p = 3
-            self.q = 5
+            print("p.txt and/or q.txt not found. Please run -g flag to generate p and q.")
+            self.p = 0
+            self.q = 0
+            
+        #calulate n and d
         self.n = self.p * self.q
 
-        #d is the modular multiplicative inverse of e mod (p-1)(q-1)
-        n = (self.p - 1) * (self.q - 1)
-        e_m_inv = BitVector(intVal=self.e).multiplicative_inverse(BitVector(intVal = n))
-        self.d = e_m_inv.int_val() % n
+        #d is the modular multiplicative inverse of e mod (p-1)(q-1) where (p-1)(q-1) is the totient of n
+        #if p and q are not set, then d is set to 0
+        if self.p == 0 or self.q == 0:
+            self.d = 0
+        else:
+            totient_n = (self.p - 1) * (self.q - 1)
+            e_m_inv = BitVector(intVal=self.e).multiplicative_inverse(BitVector(intVal = totient_n))
+            self.d = e_m_inv.int_val() % totient_n
     
     def encrypt(self, plaintext:str, ciphertext:str) -> None:
-        #read the input file
+        #read the input file and open the output file
         bv = BitVector(filename=plaintext)
         output_file = open(ciphertext, 'w')
 
+        #iterate through the file and encrypt each block
         while bv.more_to_read:
             #read 128 bits from the file and pad from right if necessary
             bitvec = bv.read_bits_from_file(128)
@@ -44,31 +57,41 @@ class RSA():
             bitvec = BitVector(intVal= int_val, size=256)
             output_file.write(bitvec.get_bitvector_in_hex())
 
+        #close the output file
         output_file.close()
     
     def decrypt(self, ciphertext:str, recovered_plaintext:str) -> None:
+        #read the input file and open the output file
         cipher_file = open(ciphertext, 'r')
+        #read entire file as hex string
         input_data = cipher_file.read()
-
         bv = BitVector(hexstring=input_data)
         output_file = open(recovered_plaintext, 'w')
 
+        #using a counter to iterate through the file and decrypt each block
+        #instead of using more_to_read because it seems that more_to_read is not working when reading from a hex file
+        #if more_to_read is used, then the program will not read the last block or will just read junk
+        #if figure out why more_to_read is not working, then change the while loop to use more_to_read for cleaner code
         i = 0
-
         while(i < bv.length()):
+            #read 256 bits from the file
             bitvec = bv[i:i+256].int_val()
+            #use the chinese remainder theorem to decrypt the block
             decrypted_int = self.exec_crt(bitvec)
+            #write the decrypted block to file
             decrypted_bv = BitVector(intVal=decrypted_int, size=256)
-
+            #remove the padding and write to file
             decrypted_text_bv = decrypted_bv[128:]
             output_file.write(decrypted_text_bv.get_bitvector_in_ascii())
+            #increment the counter by 256
             i += 256
-
+        #close and cleanup
         output_file.close()
 
         return
 
     def generate_keys(self, p_file, q_file) -> None:
+        #open p and q file for writing
         pFILE = open(p_file, 'w')
         qFILE = open(q_file, 'w')
 
@@ -77,7 +100,8 @@ class RSA():
         p = prime_gen.findPrime()
         q = prime_gen.findPrime()
 
-        #check that the leftmost bits of p and q are 1
+        #check that the leftmost 2 bits of p and q are 1
+        #if not, then generate new p and q
         p_bv = BitVector(intVal=p)
         q_bv = BitVector(intVal=q)
         while(p_bv[0] != 1 or q_bv[0] != 1 or p_bv[1] != 1 or q_bv[1] != 1):
@@ -99,13 +123,20 @@ class RSA():
         qFILE.close()
 
     def exec_crt(self, bv_int) -> int:
+        #use the chinese remainder theorem to decrypt the block
+        #execute CRT by class notes and steps in Avi Kak's lecture
+        #given c is the encrypted block,
+        #vP = c^d mod p
+        #vQ = c^d mod q
         vP = pow(bv_int, self.d, self.p)
         vQ = pow(bv_int, self.d, self.q)
+        #take the modular multiplicative inverse of q mod p and p mod q
         qI = BitVector(intVal=self.q).multiplicative_inverse(BitVector(intVal=self.p))
         xP = self.q * qI.int_val()
         pI = BitVector(intVal=self.p).multiplicative_inverse(BitVector(intVal=self.q))
         xQ = self.p * pI.int_val()
-        pt = (vP * xP + vQ * xQ) % (self.p * self.q)
+        #pt = (vP * xP + vQ * xQ) mod n
+        pt = (vP * xP + vQ * xQ) % (self.n)
         return pt
 
     def find_gcd(self, a, b) -> int:
@@ -114,18 +145,18 @@ class RSA():
         return abs(a)
 
 if __name__ == "__main__":
-    cipher = RSA(e=65537)
     flag = sys.argv[1]
     if flag == '-g':
         p_file = sys.argv[2]
         q_file = sys.argv[3]
+        cipher = RSA(e=e)
         cipher.generate_keys(p_file, q_file)
     else:
         inputfile = sys.argv[2]
         p_file = sys.argv[3]
         q_file = sys.argv[4]
         outputfile = sys.argv[5]
-        
+        cipher = RSA(e=65537)
         if flag == '-e':
             cipher.encrypt(inputfile, outputfile)
         elif flag == '-d':
